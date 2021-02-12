606b995357480adc7fd222e71f548a38058cc2ca
TalysonNascif <talyson.nascif@fatec.sp.gov.br>
2021-02-12 01:25:35 +0000

    (11/02) Início da revisão de JS

---
+++ revisao-js/vetores.js
@@ -0,0 +1,26 @@
+let frutas = ['maçã', 'laranja', 'pera', 'uva', 'mamão', 'banana']
+
+// Vetor original
+console.log(frutas)
+
+//Retirar o último elemento de um vetor
+let ultimaFruta = frutas.pop()
+
+// Vetor alterado
+console.log(frutas)
+console.log(ultimaFruta)
+
+//Remoção do prmeiro elemento do vetor
+let primeiraFruta = frutas.shift()
+
+console.log(frutas)
+console.log(primeiraFruta)
+
+// Remoção em posição intermediária
+// 1º parametro -> a posição de remoção (contagem começa em 0)
+// 2º parametro -> a quantidade de elementos a remover
+
+let terceiraFruta = frutas.splice(2, 1) // Sempre retornará um vetor
+
+console.log(frutas)
+console.log(terceiraFruta)
